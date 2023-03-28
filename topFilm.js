57a9036322293493fea13fefd67cee9f17a86ed4
Dario <dariovicentealmiron@gmail.com>
2023-03-28 10:52:22 +0200

    add some more info in topFilms array

--- topFilm.js
+++ topFilm.js
@@ -1,69 +1,99 @@
 var exports = module.exports={};
 
 exports.topFilms = [
-    {
-      id: 0,
-      title: 'Rosemary’s Baby',
-      director: 'Roman Polanski',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 1,
-      title: 'The Godfather',
-      director: 'Francis Ford Coppola',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 2,
-      title: 'The Big Lebowski',
-      director: 'Joel Coen',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 3,
-      title: 'El secreto de tus hojos',
-      director: 'Juan José Campanella',
-    },
-    {
-      id: 4,
-      title: 'Goodfellas',
-      director: 'Martin Scorsese ',
-      Genre:"Biography, Crime, Drama",
-      Plot: "The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners Jimmy Conway and Tommy DeVito in the Italian-American crime syndicate",
-      Actors:"Robert De Niro, Ray Liotta, Joe Pesci",
-      Poster:"https://m.media-amazon.com/images/M/MV5BY2NkZjEzMDgtN2RjYy00YzM1LWI4ZmQtMjIwYjFjNmI3ZGEwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg",
+  {
+    id: 0,
+    title: 'Goodfellas',
+    director: 'Martin Scorsese',
+    genre: 'Crime/Drama',
+    year: 1990,
+    plot: 'The story follows Henry Hill, a young man from Brooklyn who joins the mafia and becomes a renowned gangster in New York City.',
+    actors: ['Ray Liotta', 'Robert De Niro', 'Joe Pesci']
+  },
+  {
+     id: 1,
+    title: 'Rosemary’s Baby',
+    director: 'Roman Polanski',
+    genre: 'Horror/Drama',
+    year: 1968,
+    plot: 'The movie follows a pregnant woman who begins to suspect that something sinister is happening with her pregnancy and her neighbors.',
+    actors: ['Mia Farrow', 'John Cassavetes', 'Ruth Gordon']
+  },
+ {
+     id: 2,
+    title: 'The Big Lebowski',
+    director: 'Joel Coen',
+    genre: 'Comedy/Drama',
+    year: 1998,
+    plot: 'The Dude, a slacker in Los Angeles, is mistaken for a millionaire with the same name and is dragged into a series of problems with a group of criminals.',
+    actors: ['Jeff Bridges', 'John Goodman', 'Julianne Moore']
+  },
+{
+     id: 3,
+    title: 'El secreto de tus ojos',
+    director: 'Juan José Campanella',
+    genre: 'Drama/Crime',
+    year: 2009,
+    plot: 'A retired judicial officer begins writing a novel to deal with a rape and murder case he couldn\'t solve decades ago.',
+    actors: ['Ricardo Darín', 'Soledad Villamil', 'Pablo Rago']
+  },
+ {
+     id: 4,
+    title: 'The Godfather',
+    director: 'Francis Ford Coppola',
+    genre: 'Crime/Drama',
+    year: 1972,
+    plot: 'The movie follows the Corleone family, a powerful mafia family in New York, and how the patriarch, Vito Corleone, struggles to maintain his power and influence.',
+    actors: ['Marlon Brando', 'Al Pacino', 'James Caan']
+  },
+ {
+    id: 5,
+    title: 'Cidade de Deus',
+    director: 'Fernando Meirelles',
+    genre: 'Drama/Crime',
+    year: 2002,
+    plot: 'The movie follows the lives of several residents of a favela in Rio de Janeiro and their struggle to survive amidst violence and organized crime.',
+    actors: ['Alexandre Rodrigues', 'Leandro Firmino', 'Phellipe Haagensen']
+  },
+  
+{
+     id: 6,
+    title: 'Lock, Stock and Two Smoking Barrels',
+    director: 'Guy Ritchie',
+    genre: 'Crime/Comedy',
+    year: 1998,
+    plot: 'A group of London friends gets involved in a high-stakes poker game and ends up owing a large sum of money to a local gangster.',
+    actors: ['Jason Flemyng', 'Dexter Fletcher', 'Nick Moran']
+  },
+{
+   id: 7,
+   title: 'La comunidad',
+   director: 'Álex de la Iglesia',
+   genre: 'Comedy/Crime',
+   year: 2000,
+   plot: 'A group of neighbors find a large amount of money in an apartment and decide to split it between them, but things quickly get complicated.',
+   actors: ['Carmen Maura', 'Eduardo Antuña', 'Jesús Bonilla', 'Terele Pávez']
+},
 
-    },
-    {
-      id: 5,
-      title: 'Cidade de Deus ',
-      director: 'Fernando Meirelles y Kátia Lund',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 6,
-      title: 'Lock, Stock and Two Smoking Barrels',
-      director: 'Guy Ritchie',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 7,
-      title: 'La comunidad',
-      director: 'Álex de la Iglesia',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 8,
-      title: 'Psycho',
-      director: 'Alfred Hitchcock',
-      Genre:"Biography, Crime, Drama"
-    },
-    {
-      id: 9,
-      title: 'Pulp Fiction',
-      director: 'Quentin Tarantino',
-      Genre:"Biography, Crime, Drama"
-    },
+{
+   id: 8,
+   title: 'Psycho',
+   director: 'Alfred Hitchcock',
+   genre: 'Horror/Suspense',
+   year: 1960,
+   plot: 'A secretary steals money from her employer and goes on the run, checking into a remote motel run by a strange young man and his mother.',
+   actors: ['Anthony Perkins', 'Janet Leigh']
+},
+
+{
+   id: 9,
+   title: 'The Front',
+   director: 'Martin Ritt',
+   genre: 'Drama/Comedy',
+   year: 1976,
+   plot: 'A New York writer is pursued by the House Un-American Activities Committee in the 1950s and becomes the "front" for several scripts written by other writers blacklisted due to their political beliefs.',
+   actors: ['Woody Allen', 'Zero Mostel']
+}
   ];
   exports.users = [
     {
